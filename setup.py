2bdd8ae5c3b18e45ff29538f6ef836434b5e549b
arnaldorusso <arnaldorusso@gmail.com>
2013-10-03 14:30:00 +0200

    initial package commit

---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+setup(
+    name='Poubelle',
+    version='0.1.0',
+    description='Poubelle scripts. Some useful and useless scripts that should be on a poubelle and rewritten.',
+    long_description=readme + '\n\n' + history,
+    author='Arnaldo Russo',
+    author_email='arnaldorusso@gmail.com',
+    url='https://github.com/arnaldorusso/Poubelle',
+    packages=[
+        'Poubelle',
+    ],
+    package_dir={'Poubelle': 'Poubelle'},
+    include_package_data=True,
+    install_requires=[
+    ],
+    license="Python Software License",
+    zip_safe=False,
+    keywords='Poubelle',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+    ],
+    test_suite='tests',
+)
---
+++ tests/test_Poubelle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_Poubelle
+----------------------------------
+
+Tests for `Poubelle` module.
+"""
+
+import unittest
+
+from Poubelle import Poubelle
+
+
+class TestPoubelle(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
